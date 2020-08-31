08e8df1ffd1792bd12cfb6d1198388a673e33334
ThisIsKeb <thisiskeb@gmail.com>
2020-08-31 09:50:29 -0400

    Updates

---
+++ precache-manifest.160a7787fee833cd3195874cacc951ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b9eea5b6746c57cfa18f2d033bb416",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e9e51e2539139066e416",
+    "url": "/robofriends/static/css/main.6daaa671.chunk.css"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/robofriends/static/js/2.14bc996e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.14bc996e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e51e2539139066e416",
+    "url": "/robofriends/static/js/main.91cc48b6.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
